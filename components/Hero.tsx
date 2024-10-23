b09a07ef0ab5e6ee54d8f8f700ad34dfc4408564
Peter Wandag <gadnaw@gmail.com>
2024-10-23 14:09:34 +0800

    last version of nextjs 14

---
+++ components/Hero.tsx
@@ -0,0 +1,22 @@
+import { Spotlight } from "./ui/Spotlight";
+
+const Hero = () => {
+  return (
+    <div className="pb-20 pt-36">
+      {/* Spotlight effect - Aceternity UI */}
+      <div>
+        <Spotlight
+          className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
+          fill="white"
+        />
+        <Spotlight className="-left-full  h-[80vh] w-[50vw]" fill="purple" />
+        <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
+        <h1 className="text-black text-center text-4xl font-bold">
+          Hello World
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
