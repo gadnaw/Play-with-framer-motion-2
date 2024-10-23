e96995a041a216d1cf5741facab0d0c1664f19d8
Peter Wandag <gadnaw@gmail.com>
2024-10-23 14:24:34 +0800

    framer motion

---
+++ components/BasicsOfMotion.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BasicsOfMotion = () => {
+  return (
+    <div className="grid place-content-center h-screen gap-3">
+      <button className="example-button">Click me</button>
+    </div>
+  );
+};
+
+export default BasicsOfMotion;
